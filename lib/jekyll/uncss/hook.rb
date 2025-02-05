089a0696362e29017ac77be2be6ded671d5cae2b
Aaron Li <aaron.li@genvira.com>
2025-02-05 10:49:12 -0500

    Initialize gem

---
+++ lib/jekyll/uncss/hook.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "hook/version"
+
+module Jekyll
+  module Uncss
+    module Hook
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/jekyll/uncss/hook_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Jekyll::Uncss::Hook do
+  it "has a version number" do
+    expect(Jekyll::Uncss::Hook::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
